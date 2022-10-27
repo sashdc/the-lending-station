186d3dabe18f9f38d7538000874c6869b76c887e
honsumal <14leealastair@gmail.com>
2022-10-27 11:21:11 -0400

    added models

---
+++ models/book.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    synopsis: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+    available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    available_next: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    borrowed_user: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'book',
+  }
+);
+
+module.exports = Book;
---
+++ models/user.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection.js');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {len: [8]}        
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        }
+    },
+    borrowed_book: {
+        type: DataTypes.INTEGER,
+        unique: true,
+        references: {
+            model: 'book',
+            key: 'id'
+        }
+    },
+    admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
