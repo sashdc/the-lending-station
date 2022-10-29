9a4e264c1b6a3aa442c0c2a9e173b7deb2505953
saharsh <saharshdc@gmail.com>
2022-10-28 22:02:30 -0400

    added star rating for book

--- public/js/book.js
+++ public/js/book.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
     event.preventDefault();
 
     const review = document.querySelector('#review').value.trim();
-    const rating = document.querySelector('#rating').value;
+    const rating = bookRating;
 
     if (review) {
         const response = await fetch(`/api/book/:id/review`, {
@@ -38,6 +38,34 @@ const addButtonHandler = async (event) => {
       };
     
       
+      // star rating
+      const ratingStars = [...document.getElementsByClassName("rating__star")];
+
+      function executeRating(stars) {
+        const starClassActive = "rating__star fas fa-star";
+        const starClassInactive = "rating__star far fa-star";
+        const starsLength = stars.length;
+        let bookRating;
+        let i;
+        stars.map((star) => {
+          star.onclick = () => {
+            i = stars.indexOf(star);
+            bookRating = i + 1;
+            console.log("this many stars " + bookRating)
+      
+            if (star.className===starClassInactive) {
+              for (i; i >= 0; --i) stars[i].className = starClassActive;
+            } else {
+              for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+            }
+      
+          };
+        });
+      }
+      executeRating(ratingStars);
+      
+
+
       document
       .querySelector('.new-review-form')
       .addEventListener('submit', newFormHandler);
